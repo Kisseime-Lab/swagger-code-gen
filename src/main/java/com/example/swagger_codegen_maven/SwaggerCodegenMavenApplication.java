f4ff691f6c26d17e2ee3305dbf6e267d487213c7
Kyse7535 <ktevot@gmail.com>
2025-04-06 19:10:21 +0200

    initial commit

---
+++ src/main/java/com/example/swagger_codegen_maven/SwaggerCodegenMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.swagger_codegen_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerCodegenMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerCodegenMavenApplication.class, args);
+	}
+
+}
